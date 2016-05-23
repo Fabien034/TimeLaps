c3e7ff15023e25a7d454fafad78da28168370e80
Fabien034 <fabien.rosso@outlook.fr>
2016-05-23 16:58:09 +0200

    Modification des print

--- TransfertFichier.py
+++ TransfertFichier.py
@@ -52,6 +52,8 @@ class ThreadReception(threading.Thread):
 def main():
     raw_input("Appuyer sur une touche pour demarrer l'envoi des photos")
 
+    os.system("clear")
+
     #Lecture et recuperation des variables dans le fichier configServer
     with open("configServer", "rb") as fichierConfig:
         configRead = pickle.Unpickler(fichierConfig)
@@ -85,17 +87,16 @@ def main():
         listFile = ListDirectory(WAITPATH)
 
         if len(listFile) > 0:
-            file = Photo(listFile[0])
-
-            octets = os.path.getsize(file.pathFile) / 1024
-
+            file = Photo(listFile[0]) # Création de la class photo
+            octets = os.path.getsize(file.pathFile) / 1024 # Nombre d'octets de la photo
             print(" >> OK : '" + file.nameFile + "' [" + str(octets) + " Ko]")
-            print("")
-
+            
+            # on crée le chemin du fichier pour le serveur suivant la date de création
             datePath = time.strftime("%Y/%y.%m.%d/",file.createDate)
             pathDestination = os.path.join(HOMESERVER,datePath)
             fileDestination = os.path.join(pathDestination,file.nameFile)
 
+            # on demande au serveur de créer le chemin si il n'eiste pas
             message_emis = b"makedirs {0}".format(pathDestination)
             try:
                 # emission
@@ -104,7 +105,8 @@ def main():
             except:
                 break
 
-            message_emis = b"EnvoiPhoto {0}".format(file.nameFile)
+            # on indique au serveur que l'on va lui enoyer un fichier de x octets
+            message_emis = b"EnvoiPhoto {0} {1}".format(file.nameFile,octets)
             try:
                 # emission
                 mySocket.send(message_emis.decode(encoding='UTF-8'))
@@ -113,29 +115,32 @@ def main():
                 break
             print(" >> Envoi de la photo '{0}'".format(file.nameFile))
 
+            # subprocess SCP pour l'envoie de la photo
             subprocess.call(["scp", "-P", bytes(PORTSSH), "-p", file.pathFile ,"{0}@{1}:{2}".format(USER, bytes(HOST), fileDestination)])
 
             time.sleep(0.01)
             print(" >> Envoi OK")
-
-            # Création du dossier Date dans le répertoire de travail
+            
+            # on crée le chemin du fichier pour le rangement sur le RPi suivant la date de création
+            # si il n'existe pas on crée le dossier
             sourcePath = os.path.join(os.path.expanduser("~"),"Pictures",datePath)
             if not os.path.exists(sourcePath):
                 os.makedirs(sourcePath, mode=0o777)
-                # Creation du nouveau nom
+            # Creation du nouveau nom
             newPathFile = os.path.join(sourcePath,file.nameFile)
             # Deplace/rennome la photo
             file = file.move(newPathFile)
             print(" >> Fichier '{0}' déplacé vers '{1}'".format(file.nameFile, file.parenPathFile))
+            print("")
 
 
     mySocket.close()
-    print("envoi des photos termine")
+    print(" >> envoi des photos termine")
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("/nProgramme interrompu par l'utilisateur")
+        print("Programme interrompu par l'utilisateur")
         sys.exit(0)
 
