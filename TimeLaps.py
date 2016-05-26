0bb86ad6468a1f82e32d9f82d480e4ce9eedcf9d
Fabien034 <fabien.rosso@outlook.fr>
2016-05-26 10:48:41 +0200

    V0.1.2.4 	Enregistrement des etats des script et PID dans un dictionaire 	du fichier  etatScript

--- TimeLaps.py
+++ TimeLaps.py
@@ -1,16 +1,36 @@
 ﻿# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import os, sys, time
 import subprocess
 import pickle
-
 from wrappers import Tmux
+from fonctions import *
 
 # Vérifie si le fichier configServer Existe sinon lance FichierConfig.py
 CONFIGSERVER = os.path.join(os.path.expanduser("~"), "TimeLaps", "configServer")
 if not os.path.isfile(CONFIGSERVER):
     subprocess.call(["python", os.path.join(os.path.expanduser("~"), "TimeLaps", "FichierConfig.py")])
+
+# Création d'un dictionnaire pour l'état des scripts:
+# 0 - Script non lancé
+# 1 - Script lancé
+# 2 - demande d'arrêt
+# Deuxieme chiffre de la liste est le PID du process
+dictEtatScript = {"CapturePhoto":[0,0], "TransfertFichier":[0,0], "ListDossierAttente":[0,0]}
+
+with open("etatScript", "wb") as file:
+    configWrite = pickle.Pickler(file)
+    configWrite.dump(dictEtatScript)
+
+etatScript = {}
+with open("etatScript", "rb") as file:
+        configRead = pickle.Unpickler(file)
+        etatScript = configRead.load()
+
+for cle,valeur in etatScript.items():
+    print(cle,valeur)
+
 
 #Lecture et recuperation des variables dans le fichier configServer
 with open("configServer", "rb") as fichierConfig:
@@ -25,8 +45,24 @@ with open("configServer", "rb") as fichierConfig:
 windows = Tmux(subprocess)
 # nouvelle fenetre
 windows.newWindows("TimeLaps")
+
 # lancement du script CapturePhoto.py
-windows.sendKeys("python {0}".format(os.path.join(os.path.expanduser("~"), "TimeLaps", "CapturePhoto.py")))
+script="CapturePhoto.py"
+strScript = os.path.join(os.path.expanduser("~"), "TimeLaps", script)
+windows.sendKeys("python {0}".format(strScript))
+time.sleep(0.5)
+
+# on vérifie que le script est lancé
+processPythonPid = pythonExist(strScript)
+if type(processPythonPid) == int:
+    processPython = psutil.Process(processPythonPid)
+    print("le script {} a été démarré : {}".format(strScript,processPythonPid))
+    dictEtatScript["CapturePhoto"] = [1,processPythonPid]
+else:
+    print("le script {} n'a pas été démarré".format(strScript))
+    sys.exit()
+
+
 # on la divise verticalement
 windows.splitWindows("v")
 # on redimensionne depuis le bas
@@ -39,13 +75,57 @@ windows.selectPane("0")
 windows.splitWindows("h")
 # on redimensionne depuis le droite
 windows.resizePane("R", "10")
+
 # lancement du script TransfertFichier.py
-windows.sendKeys("python {0}".format(os.path.join(os.path.expanduser("~"), "TimeLaps", "TransfertFichier.py")))
+script="TransfertFichier.py"
+strScript = os.path.join(os.path.expanduser("~"), "TimeLaps", script)
+windows.sendKeys("python {0}".format(strScript))
+time.sleep(0.5)
+
+# on vérifie que le script est lancé
+processPythonPid = pythonExist(strScript)
+if type(processPythonPid) == int:
+    processPython = psutil.Process(processPythonPid)
+    print("le script {} a été démarré : {}".format(strScript,processPythonPid))
+    dictEtatScript["TransfertFichier"] = [1,processPythonPid]
+else:
+    print("le script {} n'a pas été démarré".format(strScript))
+    sys.exit()
+
 # on la divise verticalement
 windows.splitWindows("v")
+
 # lancement du script ListDossierAttente.py
-windows.sendKeys("python {0}".format(os.path.join(os.path.expanduser("~"), "TimeLaps", "ListDossierAttente.py")))
+script="ListDossierAttente.py"
+strScript = os.path.join(os.path.expanduser("~"), "TimeLaps", script)
+windows.sendKeys("python {0}".format(strScript))
+time.sleep(0.5)
+
+# on vérifie que le script est lancé
+processPythonPid = pythonExist(strScript)
+if type(processPythonPid) == int:
+    processPython = psutil.Process(processPythonPid)
+    print("le script {} a été démarré : {}".format(strScript,processPythonPid))
+    dictEtatScript["ListDossierAttente"] = [1,processPythonPid]
+else:
+    print("le script {} n'a pas été démarré".format(strScript))
+    sys.exit()
+
 # on attache la session
 windows.attachSession()
+
+
+# Mise à jour du dictionnaire 'dictEtatScript' dans le fichier 'etatScript'
+with open("etatScript", "wb") as file:
+    configWrite = pickle.Pickler(file)
+    configWrite.dump(dictEtatScript)
+
+etatScript = {}
+with open("etatScript", "rb") as file:
+        configRead = pickle.Unpickler(file)
+        etatScript = configRead.load()
+
+for cle,valeur in etatScript.items():
+    print(cle,valeur)
 
 
